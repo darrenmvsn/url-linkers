f39be043c291029d93a6df6573a8e616ddd0724d
Darren Melvison <caiyidamacbook1@Darren-Melvison.local>
2024-05-03 11:26:50 +1000

    first commit

--- app/(portal)/Profile/page.tsx
+++ app/(portal)/Profile/page.tsx
@@ -1,30 +1,29 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import { Avatar, Chip, Button, Input } from "@nextui-org/react";
-import { useRouter } from 'next/navigation';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import { UserAuth } from 'app/context/AuthContext';
-import { getSavedPosts, getProfileIcon, getProfileName, updateUserName } from 'app/lib/firebase';
+import { getSavedPosts, updateUserName, getProfileIcon, getProfileName } from 'app/lib/firebase';
 import InterestFolder from 'app/components/InterestFolder';
 import { getInterestsFromURL } from 'app/lib/gemini-pro';
-
+import EditModal from 'app/components/EditModal';
 const Profile = () => {
 //   const interests = ["interest1", "interest2", "interest3"];
   const [saved, setSaved] = useState([]);
   const [savedTitles, setSavedTitles] = useState([])
   const [interests, setInterests] = useState([]);
+  const [imgUrl, setImgUrl] = useState("")
+  const [rename, setRename] = useState<boolean>(false);
   const { user } = UserAuth();
   const [image, setImage] = useState<string>();
   const [editState, setEditState] = useState<string>('Display');
   const [name, setName] = useState<string>('');
 
-  const router = useRouter();
-
   useEffect(() => {
     getSavedPosts().then(posts => {
       console.log(posts);
       setSaved(posts);
-      let arr = posts.map((post)=>post.title)
+      let arr = posts.map((post)=>post?.title)
       console.log(arr)
       setSavedTitles(arr)
     }).catch(error => {
@@ -32,11 +31,16 @@ const Profile = () => {
     });
     const getInterestsFromTitle = async() => {
         const data = await getInterestsFromURL(savedTitles);
+        const url = await getProfileIcon();
         console.log(data)
         if (data) {
             setInterests(data);
+        } 
+        if (url) {
+            setImgUrl(url)
         }
     }
+    
     getInterestsFromTitle();
 
     const getProfileImage = async() => {
@@ -61,50 +65,41 @@ const Profile = () => {
     setEditState('Display');
   }
 
-  const editIcon = () => {
-    router.push('/profileSetting');
-  }
-
   return (
     <div className="container mx-auto p-4">
-      <div className="flex flex-col items-center gap-4">
-        <button onClick={editIcon}>
-          <Avatar isBordered radius="full" src={image} className="w-24 h-24" />
-        </button>
-        <div className="flex flex-row items-center justify-center">
-          {editState == 'Display' && 
-          <div className="flex flex-row items-center justify-center">
-          <h1 className="text-xl pr-2">{name}</h1>
-          <Button isIconOnly size="sm" color="foreground" onClick={editProfile} className="mr-2">
-            <EditOutlinedIcon/>
+    {rename && <EditModal setRename={setRename} />}
+    <div className="flex flex-col items-center gap-4">
+      <Avatar isBordered radius="full" src={imgUrl} className="w-24 h-24" />
+      <div className="flex flex-row items-center justify-center">
+        <h1 className="text-xl">{user.email}</h1>
+        {editState === 'Display' ? (
+          <Button auto flat onClick={editProfile} className="cursor-pointer">
+            <EditOutlinedIcon />
           </Button>
-          <p></p>
-          </div>
-          }
-          {editState == 'Edit' &&
+        ) : (
           <div className="flex flex-row items-center justify-center">
-          <Input value={name} onValueChange={setName} size="sm" className='mr-2'></Input>
-          <Button size="sm" onClick={editDone}>Done</Button>
+            <Input value={name} onChange={(e) => setName(e.target.value)} size="sm" className='mr-2' />
+            <Button size="sm" onClick={editDone}>Done</Button>
           </div>
-          }
-        </div>
-        <div className="flex flex-wrap justify-center gap-3">
-          {interests.length > 0 && interests.map((interest, index) => (
-            <Chip key={index} color="default" variant="flat">
-              {interest}
-            </Chip>
+        )}
+      </div>
+      <div className="flex flex-wrap justify-center gap-3">
+        {interests.length > 0 && interests.map((interest, index) => (
+          <Chip key={index} color="default" variant="flat">
+            {interest}
+          </Chip>
+        ))}
+      </div>
+      <div className="w-full py-6">
+        <h2 className="text-xl font-bold text-center">Saved Links</h2>
+        <div className="grid grid-cols-3 gap-4 p-4 place-items-center">
+          {saved.map((save, index) => (
+            <InterestFolder key={index} interest={save?.title} url={save?.url} />
           ))}
-        </div>
-        <div className="w-full py-6">
-          <h2 className="text-xl font-bold text-center">Saved Links</h2>
-          <div className="grid grid-cols-3 gap-4 p-4 place-items-center">
-            {saved.map((save, index) => (
-              <InterestFolder key={index} interest={save.title} url={save.url} />
-            ))}
-          </div>
         </div>
       </div>
     </div>
+  </div>
   );
 };
 
